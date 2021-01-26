f4aa652e3fe03253b891ac8694c13864867c8ef0
yurywektorovich <yurywektorovich@gmail.com>
2021-01-26 16:56:55 +0300

    removed and ignored config/dev.js

--- config/dev.js
+++
@@ -1,9 +0,0 @@
-// Don't commit this file to github!
-module.exports = {
-	googleClientID:
-		"32584965912-md8bouq35f1c1ho7ne1s38eoo5sapuu4.apps.googleusercontent.com",
-	googleClientSecret: "QGQX8AX1GCUcJsfKeSknlFtJ",
-	mongoURI:
-		"mongodb+srv://yurywektorovich:boss9lra@cluster0.u2vfo.mongodb.net/Cluster0?retryWrites=true&w=majority",
-	cookieKey: "123123123",
-};
